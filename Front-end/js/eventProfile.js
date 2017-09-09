80c7cc073d88bf62d097ea6583b7140577a8db2f
Jitae Kim <kimjt1226@gmail.com>
2017-09-09 05:45:20 -0400

    more button from govolunteer page is hard corded for now

--- Front-end/js/eventProfile.js
+++ Front-end/js/eventProfile.js
@@ -2,14 +2,12 @@
 $(window).bind("load", function() { 
   //var organizationId = getOrganizationId();
   var id = sessionStorage.getItem("id");
-  console.log(id);
   $.ajax({
           url:'https://u27x0no4t5.execute-api.us-east-1.amazonaws.com/organization/organization/'
           +encodeURIComponent(id),
           method: 'GET',
           dataType: 'json',
           success: function(getData) {
-            console.log(getData);
             document.getElementById("event-name").innerHTML = getData.Items[0].eventname.S;
             document.getElementById("desc").innerHTML = getData.Items[0].description.S;
             document.getElementById("skill").innerHTML = getData.Items[0].skills.S;
@@ -19,7 +17,7 @@ $(window).bind("load", function() {
           }
   });
   $('#back').on('click', function() {
-    storage.removeItem("id");
+    sessionStorage.removeItem("id");
     window.location.replace("govolunteer.html");
   });
 });
--- Front-end/js/govolunteer.js
+++ Front-end/js/govolunteer.js
@@ -1,26 +1,64 @@
-
 var organizationId;
 
+
 $( document ).ready(function() {
-  $.ajax({
-          url:'https://u27x0no4t5.execute-api.us-east-1.amazonaws.com/organization/organization/1',
+  $.getJSON('https://u27x0no4t5.execute-api.us-east-1.amazonaws.com/organization/organization/'
+    +encodeURIComponent(0),
+    function(data,err) {
+      if (data !== null) {
+        console.log('DB access : '+ err);
+        iterURL();
+      } else {
+        console.log('DB is empty');
+      }
+  });
+  $('#more1').on('click', function() {
+    sessionStorage.setItem("id",1);
+    window.location.replace("eventProfile.html");
+  });
+  $('#more2').on('click', function() {
+    sessionStorage.setItem("id",2);
+    window.location.replace("eventProfile.html");
+  });
+  $('#more3').on('click', function() {
+    sessionStorage.setItem("id",3);
+    window.location.replace("eventProfile.html");
+  });
+  $('#more4').on('click', function() {
+    sessionStorage.setItem("id",4);
+    window.location.replace("eventProfile.html");
+  });
+
+});
+
+function iterURL() {
+
+  //while (true) {
+  for (var i = 1; i < 5; i++) {
+    $.ajax({
+          url:'https://u27x0no4t5.execute-api.us-east-1.amazonaws.com/organization/organization/'
+          +encodeURIComponent(i),
           method: 'GET',
           dataType: 'json',
           success: function(getData) {
-            organizationId = getData.Items[0].organization.S;
-            console.log(organizationId);
-            document.getElementById("event-name").innerHTML = getData.Items[0].eventname.S;
-            document.getElementById("desc").innerHTML = getData.Items[0].description.S;
+            if (getData.Items.length != 0) {
+              organizationId = getData.Items[0].organization.S;
+              var eventname = "eventname" + organizationId;
+              var desc = "desc" + organizationId;
+              console.log('organizationID : ' + organizationId);
+              document.getElementById(eventname).innerHTML = getData.Items[0].eventname.S;
+              document.getElementById(desc).innerHTML = getData.Items[0].description.S;
+            } else {
+              //break;
+            }
           },
           error: function() {
             console.log('error loading data');
+            //break;
           }
-  });
-  $('#more').on('click', function() {
-    sessionStorage.setItem("id",organizationId);
-    window.location.replace("eventProfile.html");
-  });
-});
+    });
+  }
+}
 
 function getOrganizationId() {
   console.log(organizationId);
