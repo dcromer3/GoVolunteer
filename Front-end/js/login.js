bed4798629ca1d5f43a93c4cb8ec8c74199b1d57
Jitae Kim <kimjt1226@gmail.com>
2017-09-10 00:55:44 -0400

    login.js added, but username and password are hardcoded just for demo

---
+++ Front-end/js/login.js
@@ -0,0 +1,14 @@
+function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "jitae" && password == "1234"){
+        sessionStorage.setItem("username",username);
+        window.location.replace("addNewEvent.html");
+        //window.parent.showModal("main.html");
+        return false;
+    }
+    else{
+        alert("id or password incorrect! Try again.");
+        // Disabling fields after 3 attempts.
+    }
+}
