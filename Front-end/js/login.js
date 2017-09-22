eeb4bd03cb453b27634f73c36cf8e3c5053c3422
Jitae Kim <kimjt1226@gmail.com>
2017-09-22 15:42:24 -0400

    login.js modified to call valid user id and user password from DB to login

--- Front-end/js/login.js
+++ Front-end/js/login.js
@@ -1,16 +1,26 @@
 function login(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "jitae" && password == "1234"){
-        sessionStorage.setItem("username",username);
-        window.location.replace("addNewEvent.html");
-        //window.parent.showModal("main.html");
-        return false;
-    }
-    else{
-        alert("id or password incorrect! Try again.");
-        // Disabling fields after 3 attempts.
-    }
+
+    $.ajax({
+          url:'https://lce4l1fcek.execute-api.us-east-1.amazonaws.com/user/userlogin?userid='+encodeURIComponent(username)+'&password='+encodeURIComponent(password),
+          method: 'GET',
+          dataType: 'json',
+          success: function(getData) {
+            if (getData.uservalid == false) {
+                alert("Invalid user ID");
+            } else if (getData.passvalid == false) {
+                alert("Invalid password");
+            }else if (getData.uservalid == true && getData.passvalid == true) {
+                sessionStorage.setItem("username",username);
+                window.location.replace("addNewEvent.html");
+                return false;
+            }
+          },
+          error: function() {
+            console.log('error loading data');
+          }
+    });
 }
 
 function createId() {
